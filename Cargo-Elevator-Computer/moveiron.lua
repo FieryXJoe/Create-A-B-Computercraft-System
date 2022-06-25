353e287b32aa98e7130764d903cce56852604314
FieryXJoe <josephmsherry@comcast.net>
2022-06-25 08:11:32 -0400

    Fixed rounding and logic bug, tweaked timings
    Rounding was normal rounding, not rounding up.
    Restone was initially on, 80 seconds later being turned on again, 80 seconds later turned off
    Now it is turned on, 148 seconds later turned off, 148seconds later code loops.

--- Cargo-Elevator-Computer/moveiron.lua
+++ Cargo-Elevator-Computer/moveiron.lua
@@ -1,21 +1,22 @@
-local args = {...}
- 
-local x = math.floor((args[1]/320) + 0.5)
-print(x)
+local args = {...} --gets the argument array
+
+--the number of trips the elevator needs to make, 320 iron per trip, rounded up
+local x = math.floor((args[1]/320) + 0.99999)
+print(x)--for user to know number of trips
 for i = x,0,-1 do
-    local timerdown = os.startTimer(80)
-    while true do
+    local timerdown = os.startTimer(148)--Create a timer to deposit the items below
+    redstone.setOutput("left", true)--Start moving down
+    while true do --Basic code to read the timer
         local event, timerID = os.pullEvent("timer")
         if timerID == timerdown then
-            redstone.setOutput("left", true)
+            redstone.setOutput("left", false)--move back up
             break
         end
     end
-    local timerup = os.startTimer(80)
+    local timerup = os.startTimer(148)--delay before repeating
     while true do
         local event, timerID = os.pullEvent("timer")
-        if timerID == timerup then
-            redstone.setOutput("left", false)
+        if timerID == timerup then)
             break
         end
     end
